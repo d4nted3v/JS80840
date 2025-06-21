4df783019b44708ae637111d4bb82a5d39619d07
Dante Sandoval <dantesandoval.dev@gmail.com>
2025-06-20 22:00:42 -0300

    Creación de funciones mostrarOfertas y crearOferta

--- JS/main.js
+++ JS/main.js
@@ -6,18 +6,18 @@ function menu() {
     let salir = false;
 
     while (!salir) {
-        const opcion = parseInt(prompt(`Elija una opción:
+        const opcionMenu = parseInt(prompt(`Elija una opción:
         1 - Ver ofertas
         2 - Crear oferta
         3 - Eliminar oferta
         4 - Salir`));
 
-        switch(opcion) {
+        switch(opcionMenu) {
             case 1:
                 mostrarOfertas();
                 break;
             case 2:
-                crearOferta();
+                crearOferta(ofertasLaborales);
                 break;
             case 3:
                 eliminarOferta();
@@ -34,15 +34,57 @@ function menu() {
 
 menu();
 
+
 //Funciones 
-function mostrarOfertas(){
-    console.log(ofertasLaborales)
+function mostrarOfertas() {
+    if(ofertasLaborales.length!==0){
+
+    contador = 1;
+
+    for(let oferta of ofertasLaborales){
+    console.log(`
+        Oferta Numero: ${contador}
+        Titulo oferta: ${oferta.titulo}
+        Descripción: ${oferta.descripcion}
+        Salario: ${oferta.salario}USD
+        -----------*-----------
+`)
+contador++
+    }}else{
+        console.log("No hay ofertas laborales para visualizar")
+    }
 }
 
-function crearOferta(titulo, descripcion, salario){
+function crearOferta(ofertasLaborales){
+    let titulo = prompt("Elige un título para tu oferta");
+    let descripcion = prompt("Haz una breve descripción del puesto");
+    let salario = parseInt(prompt("Carga el salario ofrecido"));
+    let nuevaOferta = {titulo: titulo, descripcion:descripcion, salario:salario}
+    let confirmacion = parseInt(prompt(`Tu nueva oferta es la siguiente:
+        Titulo: ${titulo}
+        Descripción: ${descripcion}
+        Salario: ${salario}USD
+
+        Desea confirmar y guardar oferta?
+        1 - Si
+        2 - No
+        `))
+        if(confirmacion===2){
+            menu();
+        }else if(confirmacion===1){
+            ofertasLaborales.push(nuevaOferta)
+            console.log("Oferta creada exitosamente")
+            menu();
+        }else{
+            console.log("Debes elegir una opción válida");
+            menu();
+        }
+        
+}
+
+
+function eliminarOferta(ofertasLaborales){
 
 }
 
-function eliminarOferta(){
-
-}
+console.log(ofertasLaborales)
